65a9de2c9f8211d8ae8e301c6abf0078550b9a8e
Azhagu Selvan SP <tamizhgeek@gmail.com>
2011-04-18 01:07:25 +0530

    New files added to support the change

---
+++ scrapper/admin.py
@@ -0,0 +1,5 @@
+from tweetscrapper.twitter_users.models import *
+from django.contrib import admin
+
+admin.site.register(TwitterInfo)
+
---
+++ urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+from scrapper.views import *
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^tweetscrapper/', include('tweetscrapper.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+                       url(r'^twitter/', include('twitter_users.urls')),
+			(r'^home/', home),                       
+			(r'^index/', index),
+)
