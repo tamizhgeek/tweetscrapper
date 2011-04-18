0bb075bcbdfb168f080a02d48812137b4340c2e0
Azhagu Selvan SP <tamizhgeek@gmail.com>
2011-04-18 20:24:04 +0530

    Added the required urls for scrap and getpdf methods

--- urls.py
+++ urls.py
@@ -14,6 +14,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
                        url(r'^twitter/', include('twitter_users.urls')),
-			(r'^home/', home),                       
+			(r'^$', home),                       
 			(r'^index/', index),
+                       	(r'^scrap/', scrap),
+                       	(r'^getpdf/', getpdf),
 )
